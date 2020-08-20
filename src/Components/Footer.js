94f08bde477c7e2db20763cbcfb1446819a96f48
nasaku898 <simonlim898@hotmail.com>
2020-08-19 23:33:48 -0400

    Updated information

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,8 +19,8 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2017 Tim Baker</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>&copy; Copyright 2020 Simon Lim</li>
+              <li>Design by <a title="Tim Baker" href="http://www.timbakerdev.com">Tim Baker</a></li>
            </ul>
 
         </div>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -26,8 +26,6 @@ class Header extends Component {
             <li><a className="smoothscroll" href="#about">About</a></li>
 	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
             <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-            <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
-            <li><a className="smoothscroll" href="#contact">Contact</a></li>
          </ul>
 
       </nav>
@@ -35,7 +33,7 @@ class Header extends Component {
       <div className="row banner">
          <div className="banner-text">
             <h1 className="responsive-headline">I'm {name}.</h1>
-            <h3>I'm a {city} based <span>{occupation}</span>. {description}.</h3>
+            <h3><span>I'm a {city} based <span style={{color:"#32a2a8", fontSize:"20px"}}>{occupation}</span>. {description}. </span></h3>
             <hr />
             <ul className="social">
                {networks}
